3b9ff9cc8bd60c292677617ebdafb8430f394c33
김규현 <kgh2120@gmail.com>
2024-02-19 10:45:31 +0900

    init

---
+++ src/main/java/com/kk/jpaqueryperformance/JpaQueryPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.kk.jpaqueryperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueryPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaQueryPerformanceApplication.class, args);
+    }
+
+}
