2baf401a6cce2c3efe90ec67be181f51b23cba19
김규현 <kgh2120@gmail.com>
2024-04-25 08:26:51 +0900

    프로젝트 이름 변경

--- src/main/java/com/kk/springsql/SpringSqlApplication.java
+++ src/main/java/com/kk/springsql/SpringSqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaQueryPerformanceApplication {
+public class SpringSqlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpaQueryPerformanceApplication.class, args);
+        SpringApplication.run(SpringSqlApplication.class, args);
     }
 
 }
